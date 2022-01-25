3de8826e3d0180648db51f4001c0b620ab41e14b
unknown <mattenos4@gmail.com>
2022-01-25 18:36:00 -0500

    Updated server side js.

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /notes'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public'))
+);
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`)
 );
